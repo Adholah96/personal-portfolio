8a7d75277a8a30039386d7dc394cd773aeb4e363
Adholah96 <adholah96@gmail.com>
2023-01-30 17:49:55 +0300

    add menu window

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.menu'); 
+const windowMenu = document.querySelector('.window')
 menu.addEventListener('click', ()=>{
     menu.classList.toggle('active');
+    windowMenu.classList.toggle('active')
 })
