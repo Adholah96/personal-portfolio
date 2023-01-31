e5d2dd66a5079d62dd7e5f6cd4dd9a192fbff25c
Adholah96 <adholah96@gmail.com>
2023-01-31 12:13:05 +0300

    fix hamburger links

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector('.menu');
 const windowMenu = document.querySelector('.window');
+
 menu.addEventListener('click', () => {
   menu.classList.toggle('active');
   windowMenu.classList.toggle('active');
 });
+document.querySelectorAll('.menu-link').forEach((k) => k.addEventListener('click', () => {
+  menu.classList.remove('active');
+  windowMenu.classList.remove('active');
+}));
